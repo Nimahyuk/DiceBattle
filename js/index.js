51f31e9b94fcb2ceddf72af74749b1142c63829e
Adrián González <119411884+Nimahyuk@users.noreply.github.com>
2022-12-21 12:30:46 +0000

    Dice Battle Game
    Dice battle, where you trow tow random dice and one player win.

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function randomDice() {
+
+    var randomNumber1 = Math.floor((Math.random() * 6)) + 1;
+    var randomNumber2 = Math.floor((Math.random() * 6)) + 1;
+
+    document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector(".resultado").innerHTML = "Player 1 wins"
+    } else {
+        if (randomNumber1 < randomNumber2) {
+            document.querySelector(".resultado").innerHTML = "Player 2 wins"
+        }
+        else {
+            document.querySelector(".resultado").innerHTML = "Draw"
+        }
+    }
+}
+
+
+
+
