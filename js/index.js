2e324842b808ca9ed2e04265b3aa94f0ba35e5d5
Adrián González <119411884+Nimahyuk@users.noreply.github.com>
2022-12-22 17:08:11 +0000

    Updated with JQuery
    Implemented JQuery methods to minimize the code

--- js/index.js
+++ js/index.js
@@ -3,17 +3,17 @@ function randomDice() {
     var randomNumber1 = Math.floor((Math.random() * 6)) + 1;
     var randomNumber2 = Math.floor((Math.random() * 6)) + 1;
 
-    document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-    document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+    $(".img1").attr("src", "images/dice" + randomNumber1 + ".png");
+    $(".img2").attr("src", "images/dice" + randomNumber2 + ".png");
 
     if (randomNumber1 > randomNumber2) {
-        document.querySelector(".resultado").innerHTML = "Player 1 wins"
+        $(".resultado").text("Player 1 wins");
     } else {
         if (randomNumber1 < randomNumber2) {
-            document.querySelector(".resultado").innerHTML = "Player 2 wins"
+            $(".resultado").text("Player 2 wins");
         }
         else {
-            document.querySelector(".resultado").innerHTML = "Draw"
+            $(".resultado").text("Draw");
         }
     }
 }
